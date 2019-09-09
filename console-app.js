8af429bc8d60947da03c7e8a4a517d34aa941d8d
Janshai <jack.r.s.adams@gmail.com>
2019-09-09 23:25:36 +0100

    application prints list now and logic houed in correct files. Error if incorrect number of arguments

--- console-app.js
+++ console-app.js
@@ -5,6 +5,20 @@ const supplier = require('./supplier')
 main()
 
 function main() {
-    console.log(yargs.argv._.length)
-    supplier.getRideOptions('dave', undefined)
+    if (yargs.argv._.length < 2) {
+        console.log("Error, please provide a pickup and dropoff (longitude,lattitude)")
+        console.log("Examples:")
+        console.log("node console-app.js 51.470020,-0.454295 51.00000,1.0000")
+        console.log("npm console 51.470020,-0.454295 51.00000,1.0000")
+        return
+    }
+    supplier.getRideOptions('dave', (err, carList) => {
+        if (err) { return console.log(err) }
+
+        for (i in carList) {
+            let car = carList[i]
+            let string = car.type + " " + car.price
+            console.log(string)
+        }
+    })
 }
--- supplier.js
+++ supplier.js
@@ -1,4 +1,5 @@
 const superagent = require('superagent')
+const rideOption = require('./rideOption')
 const baseURL = 'https://techtest.rideways.com/'
 
 function getRideOptions(supplier, callback) {
@@ -7,10 +8,24 @@ function getRideOptions(supplier, callback) {
     .query({ pickup: "3.410632,-2.157533", dropoff: "51.00000,1.0000" })
     .end((err, res) => {
         if (err) {
+            return callback(err)
         }
 
-        console.log(res.body)
+        let carList = unWrapCarList(res.body)
+        callback(undefined, carList)
     })
+}
+
+function unWrapCarList(json) {
+    let options = json.options
+    let supplier = json.supplier_id
+    var carList = []
+    for (i in options) {
+        let o = options[i]
+        carList.push(new rideOption(supplier, o.car_type, o.price))
+    }
+
+    return carList
 }
 
 module.exports = {getRideOptions}
